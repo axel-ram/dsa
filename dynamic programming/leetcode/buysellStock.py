1ceb907967d096de730d97741be0fe07a76048d5
Ramnath Sharma <ramsharma.axel@gmail.com>
2020-09-19 22:08:42 +0530

    updated

---
+++ dynamic programming/leetcode/buysellStock.py
@@ -0,0 +1,14 @@
+arr = [7,1,5,3,6,4]
+
+'''
+[1] need to check maximum profit after one selling
+[2] choice - buy the stock or sell it
+[3] at any particular day - 
+    [4] 
+    [5] selling today will give profit or not
+    [6] if profit is bigger than previously calculated profit
+    [7] profit > preProfit: preProfit = profit
+    [8] we will buy stock
+
+
+'''
